3b344d18c381d8901e0e73f375e149753f347cdf
Shehab Nagy <shehab.nagy1998@gmail.com>
2019-01-14 22:02:23 +0200

    intial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
+
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CookieService} from 'ngx-cookie-service';
+import {NgxSpinnerModule} from 'ngx-spinner';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    NgxSpinnerModule
+  ],
+  providers: [CookieService],
+  entryComponents: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
